75c09b03928e262b4952e7234397b764f5b6b50f
Abdul12Ahad <abdulahad6336123@gmail.com>
2026-01-29 19:42:24 +0530

    add_ons

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://sebrain.netlify.app/',
+  baseURL: 'https://secondbrain-be-bl9z.onrender.com',
   withCredentials: true,
 });
 
